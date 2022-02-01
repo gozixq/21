4e7fa74cb24334e25fbe017669fb462d6bb894ea
gozixq <natsinee2545@gmail.com>
2022-02-01 16:17:31 +0700

    gogo

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2)
+{
+	double x_dist = min(R1->x+R1->w,R2->x+R2->w)-max(R1->x,R2->x);
+	double y_dist = min(R1->y,R2->y)-max(R1->x-R1->h,R2->x-R2->h);
+	if(x_dist <= 0 || y_dist <= 0)
+		return 0;
+	return x_dist*y_dist;
 }
